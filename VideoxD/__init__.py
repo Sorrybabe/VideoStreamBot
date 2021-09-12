a9be76230196e361b790788e4619e2572c36130a
Sorrybabe <86021848+Sorrybabe@users.noreply.github.com>
2021-09-12 23:15:20 +0530

    Update __init__.py

--- VideoxD/__init__.py
+++ VideoxD/__init__.py
@@ -3,10 +3,11 @@ from config import VIDEO_CHAT_ID
 chat_id = VIDEO_CHAT_ID
 
 print("[LOG] Starting Client Temporarily for Information")
-bot.start()
+#bot.start()
 client = bot.get_me()
 BOT_ID = client.id
 BOT_NAME = client.first_name
 BOT_USERNAME = client.username
+print(BOT_USERNAME)
 print("[LOG] Loaded Information")
-bot.stop()
+#bot.stop()
